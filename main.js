088f8a173f35632e4d47e9e11bdc8a9a7036e814
Robert Crum <gargansa@hotmail.com>
2018-10-27 15:14:25 -0600

    added api

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//make sure things are connected
 console.log("oh hey")
+
+// //What api are we calling
+var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
+var apiUrl = "https://pixabay.com/api/?key="+API_KEY;
+axios.get(apiUrl)
+.then(function (response){
+    console.log(response)
+})
