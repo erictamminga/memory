b77ec8cfaf9bd512603418242eca66fe64c6c856
Robert Crum <gargansa@hotmail.com>
2018-10-27 17:00:20 -0600

    added some photos

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 //make sure things are connected
 console.log("oh hey")
-let pictureArray = [];
+
 // //What api are we calling
 var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
-var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=halloween&image_type=illustration";
+var SearchValue = "halloween"
+var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=" + SearchValue + "&image_type=illustration&orientation=vertical";
 axios.get(apiUrl)
 .then(function (response){
-    var picture= response.data.hits[Math.floor(Math.random()*50)].webformatURL;
+    var pictureArray=[];
+    ran = Math.floor(Math.random()*20)
+    for(var i=0;i<20;i++){
+        pictureArray[i]= response.data.hits[i].largeImageURL;
+    }
+    console.log(pictureArray[0])
 
-    console.log(picture)
-    var tableData = document.getElementById("halloween");
-    console.log(tableData)
-    tableData.src = picture
-    console.log(tableData.src)
+    
+    var tableData = document.getElementById(1 + "x" + 1);
+    tableData.src = pictureArray[0]
+    var tableData = document.getElementById(1 + "x" + 2);
+    tableData.src = pictureArray[1]
+    var tableData = document.getElementById(1 + "x" + 3);
+    tableData.src = pictureArray[2]
+    var tableData = document.getElementById(1 + "x" + 4);
+    tableData.src = pictureArray[3]
+    var tableData = document.getElementById(1 + "x" + 5);
+    tableData.src = pictureArray[4]
+    
+
+
+    // var tableData2 = document.getElementById(2 + "x" + 3);
+    // tableData2.src = pictureArray[2]
+
+
+
+    console.log(response);
+})
+.catch(function (error){
+    console.log(error)
 })
