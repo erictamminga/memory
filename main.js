b5f79722812972f77d6c8dab8fff6888b82a1d7e
Robert Crum <gargansa@hotmail.com>
2018-10-27 15:21:44 -0600

    magic happened

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("oh hey")
 
 // //What api are we calling
 var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
-var apiUrl = "https://pixabay.com/api/?key="+API_KEY;
+var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=halloween&image_type=illustration";
 axios.get(apiUrl)
 .then(function (response){
     console.log(response)
