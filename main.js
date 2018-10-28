fe39ed0be3ccecc27aa61b87a06343283ad381dc
Robert Crum <gargansa@hotmail.com>
2018-10-27 22:26:10 -0600

    append working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Api
 var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
-var SearchValue = "halloween"
+var SearchValue = "winter"
 var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=" + SearchValue + "&image_type=illustration&orientation=vertical";
 
 
@@ -8,7 +8,14 @@ var dataArray = []
 dataArray = axios.get(apiUrl)
 .then(function (response){
     console.log(response)
-    elements = response.data.hits.forEach((item)=>{createImageElement(item.largeImageURL)});
+    var deck = document.querySelectorAll(".card")
+    console.log(deck)
+    var index = 0;
+    response.data.hits.forEach((item)=>{
+        var element = createImageElement(item.largeImageURL)
+        deck[index].appendChild(element)
+        index ++;
+    });
 })
 .catch(function (error){
     console.log(error)
@@ -17,7 +24,7 @@ dataArray = axios.get(apiUrl)
 function createImageElement(src){
     var img = document.createElement('img');
     img.src = src;
-    //document.querySelectorAll("#deck .card").forEach((item)=>{item.appendChild(img)})
+    console.log(src)
     console.log(img)
     return img;
 }
