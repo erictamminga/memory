a952d11cace32897c5c1d73badad5702a91cea12
Robert Crum <gargansa@hotmail.com>
2018-10-27 21:59:54 -0600

    testing selector

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
 var SearchValue = "halloween"
 var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=" + SearchValue + "&image_type=illustration&orientation=vertical";
 
+
 var dataArray = []
 dataArray = axios.get(apiUrl)
 .then(function (response){
     console.log(response)
-    return response.data.hits.forEach((item)=>{return createImageElement(item.largeImageURL)});
-}).then(function (results){
-    console.log(results)
+    elements = response.data.hits.forEach((item)=>{createImageElement(item.largeImageURL)});
 })
 .catch(function (error){
     console.log(error)
@@ -18,6 +17,7 @@ dataArray = axios.get(apiUrl)
 function createImageElement(src){
     var img = document.createElement('img');
     img.src = src;
+    //document.querySelectorAll("#deck .card").forEach((item)=>{item.appendChild(img)})
     console.log(img)
     return img;
 }
