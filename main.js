e36d7a4c5d7bbb749a23a215ea552071f48578b7
Robert Crum <gargansa@hotmail.com>
2018-10-27 21:32:50 -0600

    created Elements

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-
 //Api
 var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
 var SearchValue = "halloween"
 var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=" + SearchValue + "&image_type=illustration&orientation=vertical";
 
-axios.get(apiUrl)
+var dataArray = []
+dataArray = axios.get(apiUrl)
 .then(function (response){
-    return dataArray= response.data.hits;
+    console.log(response)
+    return response.data.hits.forEach((item)=>{return createImageElement(item.largeImageURL)});
+}).then(function (results){
+    console.log(results)
 })
 .catch(function (error){
     console.log(error)
 })
 
-
-function createImage(src){
+function createImageElement(src){
     var img = document.createElement('img');
     img.src = src;
+    console.log(img)
     return img;
 }
