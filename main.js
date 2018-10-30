2764ccf773b072963d3ce4fcbf6542095a9e8598
Robert Crum <gargansa@hotmail.com>
2018-10-29 21:13:05 -0600

    cleanup

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ var API_KEY = "10520998-dbab288ebe7e1df80d761b434"
 var SearchValue = "trees"
 var apiUrl = "https://pixabay.com/api/?key="+API_KEY + "&q=" + SearchValue + "&image_type=illustration&orientation=vertical";
 
+//setup
 var lastTwoFlippedCards = []
+var score = 0;
+var scoreElement = document.querySelector("#score")
 
-
+//api call
 var dataArray = axios.get(apiUrl)
 .then(function (response){
-    var deck = document.querySelectorAll(".grid-item")
-    let cards = [...deck];
-    
+    var cards = document.querySelectorAll(".grid-item")
+    //let cards = [...deck];
     
     var imageSet = response.data.hits;
     var images = [...imageSet];
@@ -43,7 +45,7 @@ function getTenRandomImages(array){
         array.splice(random,1)
     }
     return newArray;
- }
+}
  
 
 var cardClicked = function (e){
@@ -58,17 +60,19 @@ var cardClicked = function (e){
  }
 }
 
- function cleanMatch(array){
-    if(array[0].firstChild.src!=array[1].firstChild.src){  
+function cleanMatch(array){
+    if(array[0].firstChild.src!=array[1].firstChild.src){ 
         setTimeout(()=>{
             array[0].classList.remove("up");
             array[1].classList.remove("up");
         },200)
+    }else{//correct match
+        updateScore(100); 
     }
- }
- function resetVariables(){
-     lastTwoFlippedCards = [];
- }
+}
+function resetVariables(){
+    lastTwoFlippedCards = [];
+}
 
 function randomizeAllImages(array){
     let copyArray = [];
@@ -78,4 +82,9 @@ function randomizeAllImages(array){
         array.splice(ranNumber,1)
     }
     return copyArray;
+}
+
+function updateScore(amount){
+    score += amount
+    scoreElement.textContent = "Score: " + score;
 }
